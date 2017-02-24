1de7d983a0c12aed2d515f43881a3ea0551921a4
RobertDQuant <Robertdquant@gmail.com>
2017-02-24 14:59:41 -0500

    Update sketch.js

--- Rob_Variable4/sketch.js
+++ Rob_Variable4/sketch.js
@@ -1,20 +1,20 @@
-var circle = {
-  x:0,
+var circle = { //this is the variable for the circle
+  x:0, // this is the location of the circle
   y:200,
-  diameter: 50
+  diameter: 50 //this is the size of the circle
 };
 
-var r = 23;
+var r = 23; // this is the variable for the colors
 var g = 160;
 var b = 135;
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(600, 400); // this is the canvas size
 }
 
-function draw() {
-  background(r, g, b);
-  fill(250, 200, 200);
+function draw() { 
+  background(r, g, b); //this is the background color
+  fill(250, 200, 200); //this is the color of the circle
   ellipse(circle.x, circle.y, circle.diameter, circle.diameter);
 
-  circle.x = circle.x + 1;
+  circle.x = circle.x + 1; //this makes the circle move across the screen
 }
